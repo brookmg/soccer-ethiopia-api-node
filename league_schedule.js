80d17b60f87cc6f404dce19809adad4b070e2a3a
brookmezgu <brookmezgebu64@gmail.com>
2019-12-03 00:27:42 +0300

    update the schedule parser to the new league

--- league_schedule.js
+++ league_schedule.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const http = require('http');
 
 const testing = false;
-const SOCCER_ETHIOPIA_LEAGUE_SCHEDULE_URL = "http://www.soccerethiopia.net/ethpl-2011";
+const SOCCER_ETHIOPIA_LEAGUE_SCHEDULE_URL = "http://www.soccerethiopia.net/ethpl-2012";
 const GameStatus = {STATUS_TOOK_PLACE: 0, STATUS_CANCELLED: 1, STATUS_POSTPONED: 2, STATUS_NORMAL: 3};
 exports.GameStatus = GameStatus;
 
@@ -26,6 +26,15 @@ exports.getAllLeagueScheduleJSON = async function () {
     return JSON.stringify(await this.getAllLeagueSchedule());
 };
 
+function isItScheduleRelatedTable(table, parent) {
+    var $ = cheerio.load(table);
+    var tds = $("td");
+    parent = cheerio(parent);
+    var result = cheerio.load(tds[0]).text().includes("ሳምንት") &&
+        (!parent.hasClass("textwidget"));
+    return result;
+}
+
 /**
  * Returns all the content found in tables with class '.tablepress-id-006', repeated or not
  * @param response
@@ -34,58 +43,60 @@ exports.getAllLeagueScheduleJSON = async function () {
 function getBaseScheduleData (response) {
     let $ = cheerio.load(response);
     let returnable = [];
-    let tables = $(".tablepress-id-006");
+    let tables = $("table");
     let i = 0;
 
     tables.toArray().forEach(table => {
-        table = cheerio.load(table);
-        const rows = table('tr');
-        let currentGameDate = "";
-        rows.toArray().forEach(row => {
-            row = cheerio.load(row);
-            const tds = row('td');
-            let team_one = Object.create(null);
-            let team_two = Object.create(null);
-            let game_status = GameStatus.STATUS_NORMAL;
-            let game_time = "";
-            if (tds.length === 1) currentGameDate = tds.text();
-            else if (tds.length === 3) {
-                const td0 = cheerio.load(tds[0]);
-                const td1 = cheerio.load(tds[1]);
-                const td2 = cheerio.load(tds[2]);
+        if (isItScheduleRelatedTable(table)) {
+            table = cheerio.load(table);
+            const rows = table('tr');
+            let currentGameDate = "";
+            rows.toArray().forEach(row => {
+                row = cheerio.load(row);
+                const tds = row('td');
+                let team_one = Object.create(null);
+                let team_two = Object.create(null);
+                let game_status = GameStatus.STATUS_NORMAL;
+                let game_time = "";
+                if (tds.length === 1) currentGameDate = tds.text();
+                else if (tds.length === 3) {
+                    const td0 = cheerio.load(tds[0]);
+                    const td1 = cheerio.load(tds[1]);
+                    const td2 = cheerio.load(tds[2]);
 
-                let t1 = {name: td0.text()};
-                let t2 = {name: td2.text()};
-                game_status = getGameStatus(td1.text());
+                    let t1 = {name: td0.text()};
+                    let t2 = {name: td2.text()};
+                    game_status = getGameStatus(td1.text());
 
-                if (game_status === GameStatus.STATUS_NORMAL) {
-                    t1.score = 0;
-                    t2.score = 0;
-                    game_time = td1.text();
-                } else if (game_status === GameStatus.STATUS_TOOK_PLACE) {
-                    t1.score = td1.text().split("-")[0];
-                    t2.score = td1.text().split("-")[1];
-                } else {    //maybe if postponed
-                    game_time = td1.text();
+                    if (game_status === GameStatus.STATUS_NORMAL) {
+                        t1.score = 0;
+                        t2.score = 0;
+                        game_time = td1.text();
+                    } else if (game_status === GameStatus.STATUS_TOOK_PLACE) {
+                        t1.score = td1.text().split("-")[0];
+                        t2.score = td1.text().split("-")[1];
+                    } else {    //maybe if postponed
+                        game_time = td1.text();
+                    }
+
+                    team_one = t1;
+                    team_two = t2;
                 }
 
-                team_one = t1;
-                team_two = t2;
-            }
-
-            if (!Number.isInteger(Number.parseInt(currentGameDate[0])) && team_one.name !== undefined) {
-                let singleScheduleItem = {
-                    team_one: team_one,
-                    team_two: team_two,
-                    game_time: game_time,
-                    game_status: game_status,
-                    week: i + 1,
-                    game_date: currentGameDate
-                };
-                returnable.push(singleScheduleItem);
-            }
-        });
-        i++;
+                if (!Number.isInteger(Number.parseInt(currentGameDate[0])) && team_one.name !== undefined) {
+                    let singleScheduleItem = {
+                        team_one: team_one,
+                        team_two: team_two,
+                        game_time: game_time,
+                        game_status: game_status,
+                        week: i + 1,
+                        game_date: currentGameDate
+                    };
+                    returnable.push(singleScheduleItem);
+                }
+            });
+            i++;
+        }
     });
 
     return returnable;
@@ -145,9 +156,21 @@ exports.getThisWeekLeagueScheduleJSON = async function () {
  */
 function getThisWeekScheduleData(response) {
     let mainList = getBaseScheduleData(response);
-    let thisWeek = mainList.filter(function (item) { return item.week === 16; });
+    // clean up repeated entries
 
-    for (let i = 0; i < mainList.length; i++) {
+    mainList = mainList.filter(function (item, index, self) {
+        return index === self.findIndex(
+            (t) => (
+                t.team_one.name === item.team_one.name &&
+                t.team_two.name === item.team_two.name &&
+                t.game_date === item.game_date
+            )
+        );
+    });
+
+    let thisWeek = mainList.filter( item => true );
+
+    for (let i = 0; i < thisWeek.length; i++) {
         if (mainList[i].team_one.name === thisWeek[0].team_one.name && mainList[i].team_two.name === thisWeek[0].team_two.name) {
             thisWeek.forEach(item => item.week = mainList[i].week); //change the week property for all the items in thisWeek array
             break;
@@ -176,7 +199,7 @@ function getGameStatus (inBetweenData) {
 function readScheduleFromFile () {
     const fx = require('fs');
     try {
-        return fx.readFileSync('./league_dummy.html', 'utf-8');
+        return fx.readFileSync('./ethpl_2012.html', 'utf-8');
     } catch (e) {
         console.error("file_" , e);
         return ""
